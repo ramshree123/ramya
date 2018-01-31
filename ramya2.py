813b2f6042860a7b8a041f5fb57373cecd452c21
ramshree123 <35587884+ramshree123@users.noreply.github.com>
2018-01-31 21:53:48 +0530

    Add files via upload

---
+++ ramya2.py
@@ -0,0 +1,7 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: /home/cs2017a226/addition.py ===================
+300
+>>> 
