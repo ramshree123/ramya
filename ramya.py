686fa35879e32262540bed3ca4ee7344c107a31d
ramshree123 <35587884+ramshree123@users.noreply.github.com>
2018-01-31 21:51:50 +0530

    Add files via upload

--- ramya.py
+++ ramya.py
@@ -1,11 +1,26 @@
-#my first program
-print("Ramyashree.a.s")
-print("im me")
-print('nayana')
-print('nayana')
-print('nayana')
- 
-print('200+20')
-print(200+20)
-print('10%2')
-print(10%2)
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 300+400
+700
+>>> 
+================== RESTART: /home/cs2017a226/calculation.py ==================
+>>> 300+500
+800
+>>> 678/98
+6.918367346938775
+>>> 76*9
+684
+>>> 45-4
+41
+>>> 45-90
+-45
+>>> 
+================= RESTART: /home/cs2017a226/intialization.py =================
+<module 'random' from '/usr/lib/python3.6/random.py'>
+>>> 100
+100
+>>> 
+>>> 300
+300
+>>> 
