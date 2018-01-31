581dec0e82ccc294271e1729fea57572398ea93f
ramshree123 <35587884+ramshree123@users.noreply.github.com>
2018-01-31 21:54:12 +0530

    Add files via upload

---
+++ ramya4.py
@@ -0,0 +1,163 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: /home/cs2017a226/snake_ladder.py =================
+press q to roll q
+Traceback (most recent call last):
+  File "/home/cs2017a226/snake_ladder.py", line 11, in <module>
+    print("you got",r)
+NameError: name 'r' is not defined
+>>> 
+================= RESTART: /home/cs2017a226/snake_ladder.py =================
+press q to roll
+q
+you got 4
+your current location is 4
+4
+press q to rollq
+you got 1
+your current location is 5
+5
+press q to rollq
+you got 3
+your current location is 8
+oh! climbed the ladder to 34
+press q to rollq
+you got 4
+your current location is 38
+snake bite and your down at 9
+press q to rollq
+you got 6
+your current location is 15
+15
+press q to rollq
+you got 6
+your current location is 21
+21
+press q to roll
+you gotq 6
+your current location is 21
+21
+press q to roll
+you got 6
+your current location is 21
+21
+press q to roll q
+you got 6
+your current location is 21
+21
+press q to roll
+you got 6
+your current location is 21
+21
+press q to roll
+you got 6
+your current location is 21
+21
+press q to rollq
+you got 1
+your current location is 22
+22
+press q to rollq
+you got 6
+your current location is 28
+28
+press q to rollq
+you got 4
+your current location is 32
+32
+press q to rollq
+you got 6
+your current location is 38
+snake bite and your down at 9
+press q to rollq
+you got 3
+your current location is 12
+12
+press q to rollq
+you got 5
+your current location is 17
+17
+press q to rollq
+you got 4
+your current location is 21
+21
+press q to rollq
+you got 4
+your current location is 25
+snake bite and your down at 4
+press q to rollq
+you got 4
+your current location is 8
+oh! climbed the ladder to 34
+press q to rollq
+you got 1
+your current location is 35
+35
+press q to rollq
+you got 1
+your current location is 36
+36
+press q to rollq
+you got 5
+your current location is 41
+41
+press q to rollq
+you got 1
+your current location is 42
+42
+press q to rollq
+you got 4
+your current location is 46
+46
+press q to rollq
+you got 6
+your current location is 52
+oh! climbed the ladder to 81
+press q to rollq
+you got 2
+your current location is 83
+83
+press q to rollq
+you got 6
+your current location is 89
+snake bite and your down at 70
+press q to rollq
+you got 4
+your current location is 74
+74
+press q to rollq
+you got 4
+your current location is 78
+78
+press q to rollq
+you got 3
+your current location is 81
+81
+press q to rollq
+you got 5
+your current location is 86
+86
+press q to rollq
+you got 3
+your current location is 89
+snake bite and your down at 70
+press q to rollq
+you got 2
+your current location is 72
+72
+press q to rollq
+you got 4
+your current location is 76
+oh! climbed the ladder to 97
+press q to rollq
+you got 1
+your current location is 98
+98
+press q to rollq
+you won
+103
+>>> 
+
+    ©
